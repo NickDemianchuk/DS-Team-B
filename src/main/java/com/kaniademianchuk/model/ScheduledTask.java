0992c88d15756bf76bb2563d83115ccd89215350
Robus <git@robus.info>
2019-01-25 16:56:36 -0600

    feat: add functionality to scheduler

--- src/main/java/com/kaniademianchuk/model/ScheduledTask.java
+++ src/main/java/com/kaniademianchuk/model/ScheduledTask.java
@@ -1,17 +1,42 @@
 package com.kaniademianchuk.model;
 
 import com.kaniademianchuk.api.IIdentifiable;
+import com.kaniademianchuk.api.ITogglable;
 
+import java.util.Optional;
 import java.util.TimerTask;
 
-public abstract class ScheduledTask extends TimerTask implements IIdentifiable {
+public class ScheduledTask extends TimerTask implements IIdentifiable {
     private static Integer latestId = 0;
+    private final Manager<ITogglable> source;
+    private final Integer subjectId;
+    private final ScheduledTaskType task;
     private Integer id;
     private String name;
 
-    ScheduledTask() {
+    public ScheduledTask(Manager<ITogglable> source, int subjectId, ScheduledTaskType task) {
+        this.source = source;
+        this.subjectId = subjectId;
+        this.task = task;
         this.id = latestId++;
         this.name = String.format("Task %d", this.id);
+    }
+
+    @Override
+    public void run() {
+        Optional<ITogglable> device = this.source.getDeviceById(id);
+        if (!device.isPresent()) {
+            return;
+        }
+        if (task.equals(ScheduledTaskType.TOGGLE)) {
+            device.get().toggle();
+        } else if (task.equals(ScheduledTaskType.TURN_ON)) {
+            device.get().turnOn();
+        } else if (task.equals(ScheduledTaskType.TURN_OFF)) {
+            device.get().turnOff();
+        } else {
+            System.err.format("Unknown task '%s' on device %d in taskId:%d\n", task, subjectId, id);
+        }
     }
 
     public Integer getId() {
---
+++ src/main/java/com/kaniademianchuk/model/ScheduledTaskType.java
@@ -0,0 +1,7 @@
+package com.kaniademianchuk.model;
+
+public enum ScheduledTaskType {
+    TOGGLE,
+    TURN_ON,
+    TURN_OFF
+}
